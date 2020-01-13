a6428b71c0c8f032edff393f847774cf08f07b23
Jorge Garcia Condado <jg811@cam.ac.uk>
2020-01-13 16:43:30 +0000

    Add contour creation
    There are 3 contour created:
    - A pixelated contour by doing otsu binarization
    - A smoothed pixelated contour by using eliptical fourier descriptors
    - An an active contour that takes the smoothed pixelated contour as its first guess

---
+++ contour.py
@@ -0,0 +1,99 @@
+from molyso.generic.otsu import threshold_otsu
+from matplotlib.colors import LinearSegmentedColormap
+from skimage.segmentation import active_contour
+import matplotlib.pyplot as plt
+import numpy as np
+import pyefd
+class contour:
+    def __init__(self, im, bias, bacteria, m, pixel_size, padding):
+        self.im = im
+        self.bias = bias #otsu bias
+        self.m = m
+        self.pixel_size = pixel_size
+        self.padding = padding
+        self.colors = [(0, 0, 0), (1, 0, 0)]
+        self.cm = LinearSegmentedColormap.from_list('test', self.colors, N=np.amax(self.im))
+
+        self.boundary = self._transform_vertices(bacteria.boundary[:, :-1], bacteria)
+        self.pixelated_contour = self._px_contour()
+        self.smoothed_contour = self.smooth()
+        self.active_contour = active_contour(self.im, self.smoothed_contour, alpha=2.0, beta=20.0, w_line=-0.4, max_px_move=0.05)
+
+    def _transform_vertices(self, verts,  bacteria):
+        verts = verts - bacteria.min[:-1] # move to get non-zero values
+        verts = verts*self.m #magnification
+        verts = verts / self.pixel_size #scaling by size of pixels
+        verts = verts + self.padding # add padding
+        verts[:,[0, 1]] = verts[:,[1, 0]] #make horizontal
+        return verts
+
+    def _px_contour(self):
+        binary_image = self.im > (threshold_otsu(self.im)*self.bias)
+        rows, columns = np.where(binary_image == True)
+        prev_r = 0
+        coordinates_l = []
+        coordinates_r = []
+        for i, r in enumerate(rows):
+            if prev_r != r:
+                coordinates_l.append([columns[i]-0.5, r-0.5])
+                coordinates_l.append([columns[i]-0.5, r+0.5])
+                coordinates_r.append([columns[i-1]+0.5, prev_r-0.5])
+                coordinates_r.append([columns[i-1]+0.5, prev_r+0.5])
+                prev_r = r
+        del coordinates_r[0:2] # pop initial useless value
+        coordinates_r.append([columns[-1]+0.5, rows[-1]-0.5]) # add last value missing
+        coordinates_r.append([columns[-1]+0.5, rows[-1]+0.5]) # add last value missing
+        last_columns = np.where(rows == rows[-1])
+        for i in np.arange(0.5, columns[-1]-columns[last_columns[0][0]], 1):
+            coordinates_r.append([columns[-1]-i, rows[-1]+0.5])
+        coordinates_r.reverse() # revers to add to l
+        first_columns = np.where(rows == rows[0])
+        for i in np.arange(0.5, columns[first_columns[0][-1]] - columns[0] , 1):
+            coordinates_r.append([columns[first_columns[0][-1]]-i, rows[0]-0.5])
+        coordinates = coordinates_l + coordinates_r
+        coordinates.append(coordinates[0]) # duplicate start point
+        return np.array(coordinates)
+
+    def smooth(self):
+        locus = pyefd.calculate_dc_coefficients(self.pixelated_contour)
+        coeffs = pyefd.elliptic_fourier_descriptors(self.pixelated_contour, order=10)
+        contour = pyefd.reconstruct_contour(coeffs, locus=locus, num_points=100)
+        return contour
+
+    def show_pixelated_contour(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.pixelated_contour[:, 0], self.pixelated_contour[:, 1], 'y', lw=1, label="Pixelated boundary")
+        ax.plot(self.boundary[:,0], self.boundary[:, 1], 'g', lw=1, label="Ground Truth")
+        plt.imshow(self.im, origin='lower', cmap=self.cm)
+        plt.title("Pixelated Boundary")
+        plt.legend(fontsize="x-small")
+        plt.show()
+
+    def show_smoothed_contour(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.smoothed_contour[:, 0], self.smoothed_contour[:, 1], 'y', lw=1, label="Smoothed boundary")
+        ax.plot(self.boundary[:,0], self.boundary[:, 1], 'g', lw=1, label="Ground Truth")
+        plt.imshow(self.im, origin='lower', cmap=self.cm)
+        plt.title("Smoothed Boundary")
+        plt.legend(fontsize="x-small")
+        plt.show()
+
+    def show_active_contour(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.active_contour[:, 0], self.active_contour[:, 1], 'y', lw=1, label="Active Contour")
+        ax.plot(self.boundary[:,0], self.boundary[:, 1], 'g', lw=1, label="Ground Truth")
+        plt.imshow(self.im, origin='lower', cmap=self.cm)
+        plt.title("Active Contour")
+        plt.legend(fontsize="x-small")
+        plt.show()
+
+    def show_contours(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.pixelated_contour[:, 0], self.pixelated_contour[:, 1], 'y', lw=2, label="Pixelated boundary")
+        ax.plot(self.smoothed_contour[:, 0], self.smoothed_contour[:, 1], 'r', lw=2, label="Smoothed boundary")
+        ax.plot(self.active_contour[:, 0], self.active_contour[:, 1], 'b', lw=2, label="Active Contour")
+        ax.plot(self.boundary[:,0], self.boundary[:, 1], 'g', lw=2, label="Ground Truth")
+        plt.imshow(self.im, origin='lower', cmap=self.cm)
+        plt.title("Contours")
+        plt.legend(fontsize="x-small")
+        plt.show()
--- examples/contour_example.py
+++ examples/contour_example.py
@@ -1,16 +1,10 @@
-import numpy as np
+from contour import contour
 import os
-import sys
-sys.path.append('../')
-from molyso.generic.otsu import threshold_otsu
-import matplotlib.pyplot as plt
-from matplotlib.path import Path
-import matplotlib.patches as patches
-from skimage.segmentation import active_contour
-from bacteria_model import bacteria_spline
+import numpy as np
 import re
+from bacteria_model import bacteria_spline
 
-directory = "../dataset/data/"
+directory = "dataset/data/"
 files = os.listdir(directory)[:1]
 im_stack = np.empty(shape=(len(files), 60, 26))
 r, l, R, theta = [float(s) for s in re.findall(r"\d+\.\d+", files[0])]
@@ -18,6 +12,10 @@ r, l, R, theta = [float(s) for s in re.findall(r"\d+\.\d+", files[0])]
 ex_wv = 0.8  # emmitted wavelength by microscope for excitation
 em_wv = 0.59  # emitted wavelength due to fluorescence
 n_b = 0
+
+m = 40
+pixel_size = 4.4
+padding = 2
 
 def spline_fn_curvature(x):
     return np.sqrt(R**2 - (x-l/2)**2) - np.sqrt(R**2-l**2/4)
@@ -27,58 +25,10 @@ bacteria = bacteria_spline(r, l, 0.01, spline_fn_curvature, theta, ex_wv, em_wv,
 for i, f in enumerate(files):
     im = np.load(directory + f)
     im_stack[i, :, :] = im
-
-def initial_contour(im):
-    binary_image = im > (threshold_otsu(im)*0.8)
-    rows, columns = np.where(binary_image == True)
-    prev_r = 0
-    coordinates_l = []
-    coordinates_r = []
-    for i, r in enumerate(rows):
-        if prev_r != r:
-            coordinates_l.append([columns[i], r])
-            coordinates_r.append([columns[i-1], prev_r])
-            prev_r = r
-    coordinates_r.pop(0) # pop initial useless value
-    coordinates_r.append([columns[-1], rows[-1]]) # add last value missing
-    coordinates_r.reverse() # revers to add to l
-    coordinates = coordinates_l + coordinates_r
-    coordinates.append(coordinates[0]) # duplicate start point
-    return coordinates
-
-m = 40
-pixel_size = 4.4
-padding = 2
-
-def transform_vertices(verts, bacteria):
-    verts = verts - bacteria.min[:-1] # move to get non-zero values
-    verts = verts*m #magnification
-    verts = verts / pixel_size #scaling by size of pixels
-    verts = verts + padding # add padding
-    verts[:,[0, 1]] = verts[:,[1, 0]] #make horizontal
-    return verts
-
-for i, im in enumerate(im_stack):
-    rm_indices = np.where(im==0.0)[0]
-    im = np.delete(im, rm_indices, axis=0)
-    init_contour = initial_contour(im)
-    fig, ax = plt.subplots()
-    codes = [Path.MOVETO] + [Path.LINETO] * (len(init_contour) - 2) + [Path.CLOSEPOLY]
-    path = Path(init_contour, codes)
-    patch = patches.PathPatch(path, fill=False, lw=2, ec = 'orange')
-    ax.add_patch(patch)
-    init_contour.pop()
-    init_contour = [val for val in init_contour for _ in range(10)]
-    act_contour = active_contour(im, np.array(init_contour))
-    codes = [Path.MOVETO] + [Path.LINETO] * (len(act_contour) - 2) + [Path.CLOSEPOLY]
-    path = Path(act_contour, codes)
-    patch = patches.PathPatch(path, fill=False, lw=2, ec = 'red')
-    ax.add_patch(patch)
-    gt_contour = bacteria.boundary[:, :-1]
-    gt_contour = transform_vertices(gt_contour, bacteria)
-    codes = [Path.MOVETO] + [Path.LINETO] * (len(gt_contour) - 1)
-    path = Path(gt_contour, codes)
-    patch = patches.PathPatch(path, fill=False, lw=2, ec = 'green')
-    ax.add_patch(patch)
-    plt.imshow(im)
-    plt.show()
+rm_indices = np.where(im==0.0)[0]
+im = np.delete(im, rm_indices, axis=0)
+contour = contour(im, 0.8, bacteria, m, pixel_size, padding)
+contour.show_pixelated_contour()
+contour.show_smoothed_contour()
+contour.show_active_contour()
+contour.show_contours()
