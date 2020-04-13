6df2409f464cdc4dafdd02510076d5fb0f654a59
Jorge Garcia Condado <jg811@cam.ac.uk>
2020-04-13 18:06:56 +0200

    Add normalization and blur to metric

--- calibration/comparison.py
+++ calibration/comparison.py
@@ -1,11 +1,18 @@
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter
 
-def compare_images(image1, image2):
-    if image1.shape != image2.shape:
+def compare_images(original_im, generated_im, blur = True, sigma = 1.0):
+    if original_im.shape != generated_im.shape:
         raise ValueError("Images must be the same shape")
-    diff = image1-image2
+    if blur:
+        orignal_im = gaussian_filter(original_im, sigma = sigma)
+        generated_im = gaussian_filter(generated_im, sigma = sigma)
+    #normalize images
+    original_im = (original_im-np.amin(original_im))/(np.amax(original_im)-np.min(original_im))
+    generated_im = (generated_im-np.amin(generated_im))/(np.amax(generated_im)-np.min(generated_im))
+    diff = original_im-generated_im
     square_diff = np.square(diff)
-    sum_image1 = np.sum(image1)
+    sum_org = np.sum(original_im)
     sum_square_diff = np.sum(square_diff)
-    metric = sum_square_diff/sum_image1
+    metric = sum_square_diff/sum_org
     return metric
