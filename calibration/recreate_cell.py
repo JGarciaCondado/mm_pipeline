547a2091d58dc2664ef3245575ca5b8022597027
Jorge Garcia Condado <jg811@cam.ac.uk>
2020-04-30 18:04:56 +0200

    Changed example of cell parameter extraction
    Displays:
    -Boundary extraction and centerline extraction
    -Spline
    -Splien approximation
    -Rotation and crop of image
    -Horizontal distribution
    -Matching r with FWHM
    -Distirbution fitting
    -Model fitting
    -Matching r and sigma with KS test
    -Image with different models

--- calibration/recreate_cell.py
+++ calibration/recreate_cell.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append('../')
 
+from extraction import get_centerline, clean_centerline, debranch_centerline, calculate_theta, calculate_l, convert_centerline, calculate_R, calculate_r, horizontal_probability_density, fwhm, grid_search_r_sigma
+from rmatching import match_r_and_psf, convolved_circle_px, cauchy, convolve_cauchy, optimum_r_and_psf, optimum_cauchy, model_cauchy, model
+
+#TODO use new BacteriaModel
+
+#TODO check how many of this do we need
 from contour import contour_real
 from shapely.geometry import Polygon
 from centerline.geometry import Centerline
@@ -10,250 +16,270 @@ from bacteria_model import SpherocylindricalBacteria
 from microscope_models import Microscope
 from scipy import ndimage, misc
 from tqdm import tqdm
+from scipy.interpolate import splrep, sproot, splev
+from scipy.optimize import curve_fit
+from microscope_models import Microscope
+from models import Microscope, SpherocylindricalBacteria
 
-cell = np.load("Cell_test_2.npy")
-rm_indices = np.where(cell==0.0)[0]
-cell = np.delete(cell, rm_indices, axis=0)
-shape = cell.shape
-contour = contour_real(cell, 1.0)
-active_contour = contour.active_contour
-boundary = Polygon(active_contour).buffer(0)
-centroid = (boundary.centroid.x, boundary.centroid.y)
-[minx, miny, maxx, maxy] = boundary.bounds
-print(centroid)
-centerline = Centerline(boundary)
-fig, ax = plt.subplots()
-ax.plot(contour.active_contour[:, 0], contour.active_contour[:, 1], 'y', lw=1, label="Active Contour")
-end_points = []
-spline = []
-branch = []
-for line in list(centerline.geoms):
-    coordinates = list(line.coords)
-    for coor in coordinates:
-        if coor not in branch:
-            if coor not in spline:
-                if coor not in end_points:
-                    end_points.append(coor)
-                else:
-                    end_points.remove(coor)
-                    spline.append(coor)
-            else:
-                spline.remove(coor)
-                branch.append(coor)
-    x, y = zip(*coordinates)
-    ax.plot(x, y, 'r')
-
-for coor in end_points:
-    ax.scatter(coor[0], coor[1], c='b')
-
-for coor in branch:
-    ax.scatter(coor[0], coor[1], c='g')
+#TODO label an clean up all of the graphs + titles
+#TODO also nicely print all values it calculates
 
 
-min_coor = (1000.0, 1000.0)
-max_coor = (0.0, 0.0)
-for branch_coor in branch:
-    if branch_coor[1] < min_coor[1]:
-        min_coor = branch_coor
-    if branch_coor[1] > max_coor[1]:
-        max_coor = branch_coor
-end_branches = [max_coor, min_coor]
-print(min_coor)
+#TODO create a single array/tuple to hold all parameters so can be fed easily to other functions
 
-if branch != []:
-    for end_coor in end_points:
-        distanceToBranch = 1000
-        closest_branch = 0.0
-        for branch_coor in branch:
-            if distanceToBranch > np.sqrt((end_coor[0]-branch_coor[0])**2 + (end_coor[1]-branch_coor[1])**2):
-                distanceToBranch = np.sqrt((end_coor[0]-branch_coor[0])**2 + (end_coor[1]-branch_coor[1])**2)
-                close_branch = branch_coor
-        if distanceToBranch < 4.0 or close_branch not in end_branches:
-            remove_coor = []
-            for spline_coor in spline:
-                if np.sqrt((end_coor[0]-spline_coor[0])**2 + (end_coor[1]-spline_coor[1])**2) < distanceToBranch:
-                    remove_coor.append(spline_coor)
-                    ax.scatter(spline_coor[0], spline_coor[1], c='m')
-            for coor in remove_coor:
-                spline.remove(coor)
-        # need to create then endpoints as branch // maybe pop the end_points?
-        else:
-            if end_coor[1] < min_coor[1]:
-                min_coor = end_coor
-            if end_coor[1] > max_coor[1]:
-                max_coor = end_coor
-else:
-    branch = end_points
-
-print(min_coor)
-
-plt.imshow(cell)
-plt.show()
-
-for branch_coor in branch:
-    spline.append(branch_coor)
-
-m = 40
-pixel_size = 4.4
-
-x, y = zip(*spline)
-plt.scatter(x, y)
-plt.axis('scaled')
-plt.show()
-
-theta = np.arctan((max_coor[0]-min_coor[0])/(max_coor[1]-min_coor[1]))
-print(theta*180/np.pi)
-rotation_matrix = np.array(((np.cos(theta), -np.sin(theta)), (np.sin(theta), np.cos(theta))))
-l = (max_coor[1]-min_coor[1])*pixel_size/m
-print(l)
-for i, coor in enumerate(spline):
-    new_coor = np.subtract(coor,min_coor)
-    new_coor = rotation_matrix.dot(new_coor)
-    new_coor = new_coor*pixel_size/m
-    new_coor[1] = new_coor[1]-l/2
-    new_coor = new_coor[::-1]
-    spline[i] = new_coor
-
-def fn(R, x):
-    return np.sqrt(R**2 - x**2) - np.sqrt(R**2-(l**2)/4)
-
-R = 100
-n = 100
-dr = 5
-old_error = 10000000
-for i in range(n):
-    error = 0.0
-    for x,y in spline:
-        error += abs(fn(R, x) - y)
-    if error > old_error:
-        dr = -dr/2
-    old_error = error
-    R = R-dr
-print(R)
-
-#Show spline
-x, y = zip(*spline)
-plt.scatter(x, y)
-x = np.arange(-l/2, l/2, 0.01)
-plt.plot(x, [fn(R, i) for i in x], 'k')
-plt.axis('scaled')
-plt.show()
-
-ex_wv = 0.8  # emmitted wavelength by microscope for excitation
-em_wv = 0.59  # emitted wavelength due to fluorescence
+# Microscope constants
 pixel_size = 4.4  # pixel size
 NA = 0.95  # Numerical aperture
-magnification = 40  # magnification
-from scipy.interpolate import splrep, sproot, splev
+m = 40  # magnification
+ex_wv = 0.8
+em_wv = 0.59
 
-class MultiplePeaks(Exception): pass
-class NoPeaksFound(Exception): pass
+#Load image
+cell = np.load("Cell_test_1.npy")
+rm_indices = np.where(cell==0.0)[0]
+cell = np.delete(cell, rm_indices, axis=0)
 
-def fwhm(x, y, k=3):
-    """
-    Determine full-with-half-maximum of a peaked set of points, x and y.
+#shape of image
+shape = cell.shape
+length = shape[0]
 
-    Assumes that there is only one peak present in the datasset.  The function
-    uses a spline interpolation of order k.
-    """
+#Obtain contour of cell
+contour = contour_real(cell, 1.0)
+active_contour = contour.active_contour
+boundary = Polygon(active_contour).buffer(0) #Fixes any holes in polygon
+[minx, miny, maxx, maxy] = boundary.bounds
 
-    half_max = np.max(y)/2.0
-    s = splrep(x, y - half_max, k=k)
-    roots = sproot(s)
+#Centroid of cell
+centroid = (boundary.centroid.x, boundary.centroid.y)
 
-    if len(roots) > 2:
-        raise MultiplePeaks("The dataset appears to have multiple peaks, and "
-                "thus the FWHM can't be determined.")
-    elif len(roots) < 2:
-        raise NoPeaksFound("No proper peaks were found in the data set; likely "
-                "the dataset is flat (e.g. all zeros).")
-    else:
-        return abs(roots[1] - roots[0])
+#Obtain centerline
+centerline = get_centerline(boundary)
 
-r_final = 0.5
-microscope = Microscope(magnification, NA, ex_wv, em_wv, pixel_size)
-
-if R > l*4:
-    print("Adjusting r")
-    fig = plt.figure(figsize=(10, 2))
-    ax1, ax2 = fig.subplots(1, 2)
-    cell_rot = ndimage.rotate(cell, -theta*180/np.pi, reshape=False)
-    cell_rot = cell_rot.astype('float')
-    cell_rot[cell_rot == 0.0] = np.nan
-    ax1.imshow(cell, cmap='gray')
-    ax1.set_axis_off()
-    ax2.imshow(cell_rot, cmap='gray')
-    ax2.set_axis_off()
-    fig.set_tight_layout(True)
-    plt.show()
-    y_max, y_min = int(max_coor[1]), int(min_coor[1])
-    print(y_max, y_min)
-    plt.imshow(cell_rot[y_min:y_max, :])
-    plt.show()
-    counts = np.nanmean(cell_rot[y_min:y_max, :], axis=0)
-    counts = (counts - np.min(counts)) / (np.max(counts) - np.min(counts))
-    plt.hist(np.arange(0, len(counts)), weights=counts, bins=np.arange(-0.5, len(counts)+1))
-    spl = splrep(range(len(counts)), counts)
-    x2 = np.linspace(0, len(counts), 100)
-    y2 = splev(x2, spl)
-    plt.plot(x2, y2)
-    plt.show()
-    print(counts)
-    org = fwhm(range(len(counts)), counts)
-    print(org)
-    r_values = np.arange(0.15, 0.6, 0.01)
-    prev_fwhm = 0.0
-    for r in tqdm(r_values):
-        bacteria = SpherocylindricalBacteria(r, l, R, theta*180/np.pi, ex_wv, em_wv, 10000)
-        #TODO optional paddic and also change centroid so calculation so can do cropping before
-        image = microscope.image_bacteria_conv(bacteria, centroid, shape)
-        rm_indices = np.where(image==0.0)[0]
-        image = np.delete(image, rm_indices, axis=0)
-        im_rot = ndimage.rotate(image, -theta*180/np.pi, reshape=False)
-        im_rot = im_rot.astype('float')
-        im_rot[im_rot == 0.0] = np.nan
-        counts = np.nanmean(im_rot[y_min:y_max, :], axis=0)
-        counts = (counts - np.min(counts)) / (np.max(counts) - np.min(counts))
-        current_fwhm = fwhm(range(len(counts)), counts)
-        if current_fwhm > org:
-            r_final = r - 0.01*((current_fwhm-org)/(current_fwhm-prev_fwhm))
-            #TODO plot both together
-            plt.hist(np.arange(0, len(counts)), weights=counts, bins=np.arange(-0.5, len(counts)+1))
-            spl = splrep(range(len(counts)), counts)
-            x2 = np.linspace(0, len(counts), 100)
-            y2 = splev(x2, spl)
-            plt.plot(x2, y2)
-            plt.show()
-            print(r_final)
-            break
-        prev_fwhm = current_fwhm
-
+#Plot image with centerline and boundary
 fig, ax = plt.subplots()
 ax.plot(contour.active_contour[:, 0], contour.active_contour[:, 1], 'y', lw=1, label="Active Contour")
 
-bacteria = SpherocylindricalBacteria(r_final, l, R, theta*180/np.pi, ex_wv, em_wv, 3000)
-verts = bacteria.boundary[:, :-1]
-#verts = verts - bacteria.min[:-1] # move to get non-zero values
-verts = verts*m #magnification
-verts = verts / pixel_size #scaling by size of pixels
-verts = verts + (centroid[1], centroid[0]) # add padding
-verts[:,[0, 1]] = verts[:,[1, 0]] #make horizontal
-ax.plot(verts[:,0], verts[:, 1], 'g', lw=1, label="Ground Truth")
-plt.imshow(cell)
+#Plot line segments that make up the centerline 
+for line in list(centerline.geoms):
+    x, y = zip(*list(line.coords))
+    ax.plot(x, y, 'r')
+ax.plot(x, y, 'r', label = 'centerline')
+
+#TODO centerline split into its components with names
+#Clean centerline
+centerline = clean_centerline(centerline)
+
+#Place end_points and branches
+coor_x, coor_y = zip(*centerline[0])
+ax.scatter(coor_x, coor_y, c='b', label="end points")
+
+coor_x, coor_y = zip(*centerline[2])
+ax.scatter(coor_x, coor_y, c='g', label="branch points")
+
+#Debranch centerline
+centerline = debranch_centerline(centerline)
+
+#Plot removed coordinates
+spline_x, spline_y = zip(*centerline[2])
+ax.scatter(spline_x, spline_y, c='m', label="debranch points")
+
+#Plot over original image
+plt.imshow(cell, origin='lower')
+plt.title("Cell boundary and centerline")
+plt.legend()
 plt.show()
-bacteria.plot_2D()
 
-image = microscope.image_bacteria_conv(bacteria, centroid, shape)
-rm_indices = np.where(image==0.0)[0]
-image = np.delete(image, rm_indices, axis=0)
+#TODO make sure that image shown has (0,0) in botom left hand corner for all
 
-#subplot(r,c) provide the no. of rows and columns
-f, axarr = plt.subplots(1,2)
+#Plot spline left
+[spline, [max_coor, min_coor], remove_coords] = centerline
+y_max, y_min = int(max_coor[1]), int(min_coor[1])
+x, y = zip(*spline)
+plt.scatter(x, y)
+plt.title("Spline points")
+plt.ylabel('Pixels')
+plt.xlabel('Pixels')
+plt.axis('scaled')
+plt.show()
 
-# use the created array to output your multiple images. In this case I have stacked 4 images vertically
-# use unified colormap max value of 700?
+#Obtain theta
+theta = calculate_theta(centerline)
+
+#Obtain l
+l = calculate_l(centerline, pixel_size, m)
+
+#Reduce centerline to new coordinate system
+centerline = convert_centerline(centerline, theta, pixel_size, m, l)
+
+#Calculate R
+R = calculate_R(centerline, l)
+
+#Show spline
+x, y = zip(*centerline)
+plt.scatter(x, y)
+x = np.arange(-l/2, l/2, 0.01)
+def fn(R, x):
+    return np.sqrt(R**2 - x**2) - np.sqrt(R**2-(l**2)/4)
+plt.plot(x, [fn(R, i) for i in x], 'k')
+plt.title('Curvature fit')
+plt.ylabel('micrometers')
+plt.xlabel('micrometers')
+plt.axis('scaled')
+plt.show()
+
+#TODO example that has big R seperately
+#Plot rotated image
+cell_rot = ndimage.rotate(cell, -theta, reshape=False)
+cell_rot = cell_rot.astype('float')
+cell_rot[cell_rot == 0.0] = np.nan
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
+plt.imshow(cell)
+plt.subplot(1, 2, 2)
+plt.title("Rotated Image")
+plt.imshow(cell_rot)
+plt.show()
+
+#Plot image with caps taken off
+#TODO y_min and y_max have they been rotated properly
+#TODO l is too big
+plt.title("Rotated and cropped caps")
+plt.imshow(cell_rot[y_min:y_max, :])
+plt.show()
+
+
+#Plot probability density histogram
+original_px = horizontal_probability_density(cell_rot[y_min:y_max, :])
+bins = np.arange(-0.5, len(original_px))
+plt.hist(bins[:-1], bins, weights=original_px, label="Discrete Experimental PDF")
+
+#Spline approximation of histogram
+spl = splrep(range(len(original_px)), original_px)
+xorg = np.linspace(0, len(original_px), 100)
+yorg = splev(xorg, spl)
+h = xorg[np.argmax(yorg)]
+plt.plot(xorg, yorg, label="B-spline approximation of PDF")
+plt.legend()
+plt.title("Cell horizontal experimental PDF with noise removed")
+plt.show()
+
+# Calculate r without matching PSF
+r = calculate_r(cell, l, R, theta, shape, y_max, y_min, centroid)
+
+# Image bacteria
+bacteria = SpherocylindricalBacteria(l-r,r, R, theta, 800,  ex_wv, em_wv)
+microscope = Microscope(m, NA, ex_wv, em_wv, pixel_size)
+image = microscope.image_bacteria(bacteria, centroid, shape)
+im_rot = ndimage.rotate(image, -theta, reshape=False)
+im_rot = im_rot.astype('float')
+im_rot[im_rot == 0.0] = np.nan
+
+
+#Compare distribution
+image_model_px = horizontal_probability_density(im_rot[y_min:y_max, :])
+plt.hist(bins[:-1], bins, weights=image_model_px, label="Model Discrete Experimental PDF")
+spl = splrep(range(len(image_model_px)), image_model_px)
+xim = np.linspace(0, len(image_model_px), 100)
+yim = splev(xim, spl)
+plt.plot(xim, yim, label="Model B-spline approximation of PDF")
+plt.plot(xorg, yorg, label="Experimental B-spline approximation of PDF")
+plt.title('Model image horizontal PDF with noise removed and matched FWHM')
+#TODO fix axe positioning
+plt.legend()
+plt.show()
+
+
+#Images
+plt.subplot(1,2,1)
+plt.title("Original image")
+plt.imshow(cell)
+plt.subplot(1,2,2)
+plt.title("Model image")
+plt.imshow(image)
+plt.show()
+
+#Get optmal gaussian
+x = range(len(original_px))
+def gaus(x, x0,sigma):
+    return np.exp(-(x-x0)**2/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+
+mu_org = np.sum([px*i for i, px in enumerate(original_px)])
+std_dev_org = np.sum([px*(i-mu_org)**2 for i, px in enumerate(original_px)])
+org_fwhm = fwhm(x, original_px)
+popt, pcov = curve_fit(gaus, x, original_px, p0 = [h, std_dev_org])
+poptc, pcovc = curve_fit(cauchy, x, original_px, p0 = [org_fwhm/2, h])
+
+# Plot all different distribution
+plt.plot(xorg, yorg, label='original')
+plt.plot(np.arange(0, 26, 0.01), gaus(np.arange(0,26,0.01), popt[0], popt[1]), label='gaussian best fit')
+plt.plot(np.arange(0, 26, 0.01), cauchy(np.arange(0,26,0.01), org_fwhm/2, poptc[1]), label='cauchy matched fwhm')
+plt.plot(np.arange(0, 26, 0.01), cauchy(np.arange(0,26,0.01), poptc[0], poptc[1]), label='cauchy best fit')
+plt.legend()
+plt.show()
+
+# Plot all different distribution
+rc, gc, hc = optimum_cauchy(original_px)
+rg, gg, hg = optimum_r_and_psf(original_px)
+plt.plot(xorg, yorg, label='original')
+plt.plot(np.arange(0, 26, 0.01), model_cauchy(np.arange(0,26, 0.01), rc, hc, gc), label="cauchy model")
+plt.plot(np.arange(0, 26, 0.01), model(np.arange(0,26, 0.01), rg, hg, gg), label="gaussian model")
+plt.legend()
+plt.show()
+#Find optimum with density matching
+opt_r, opt_sigma = grid_search_r_sigma(original_px, l, R, theta, shape, y_max, y_min, centroid, microscope, ex_wv, em_wv)
+# Image bacteria
+bacteria = SpherocylindricalBacteria(l-opt_r,opt_r, R, theta, 1700,  ex_wv, em_wv)
+microscope = Microscope(m, NA, ex_wv, em_wv, pixel_size)
+image = microscope.image_bacteria(bacteria, centroid, shape, sigma=opt_sigma)
+im_rot = ndimage.rotate(image, -theta, reshape=False)
+im_rot = im_rot.astype('float')
+im_rot[im_rot == 0.0] = np.nan
+
+
+#Compare distribution
+image_model_px = horizontal_probability_density(im_rot[y_min:y_max, :])
+plt.hist(bins[:-1], bins, weights=image_model_px, label="Model Discrete Experimental PDF")
+spl = splrep(range(len(image_model_px)), image_model_px)
+xim = np.linspace(0, len(image_model_px), 100)
+yim = splev(xim, spl)
+plt.plot(xim, yim, label="Model B-spline approximation of PDF")
+plt.plot(xorg, yorg, label="Experimental B-spline approximation of PDF")
+plt.title('Model image horizontal PDF with noise removed and optimum r and sigma')
+#TODO fix axe positioning
+plt.legend()
+plt.show()
+
+
+bacteria = SpherocylindricalBacteria(l-rc/11,rc/11, R, theta, 2000,  ex_wv, em_wv)
+im_c = microscope.image_bacteria_cauchy(bacteria, centroid, shape, gamma=gc, noise=200)
+
+#Images
+plt.subplot(2,3,1)
+plt.title("Original image")
+plt.imshow(cell)
+plt.subplot(2,3,2)
+plt.title("Model image, gaussian")
+plt.imshow(image)
+plt.subplot(2,3,3)
+plt.title("Model image, cauchy")
+plt.imshow(im_c)
 cell = (cell-np.min(cell))/(np.max(cell)-np.min(cell))
 image = (image-np.min(image))/(np.max(image)-np.min(image))
-axarr[0].imshow(cell)
-axarr[1].imshow(image)
+im_c = (im_c-np.min(im_c))/(np.max(im_c)-np.min(im_c))
+plt.subplot(2,3,4)
+plt.imshow(cell)
+plt.subplot(2,3,5)
+plt.imshow(image)
+plt.subplot(2,3,6)
+plt.imshow(im_c)
+plt.show()
+
+
+#Plot image with measured boundary and new boundary
+fig, ax = plt.subplots()
+ax.plot(contour.active_contour[:, 0], contour.active_contour[:, 1], 'y', lw=1, label="Active Contour")
+bacteria_boundary = np.array(list(map(list, bacteria.boundary)))
+verts = microscope._transform_vertices(bacteria_boundary, bacteria, centroid)
+ax.plot(verts[:,0], verts[:, 1], 'g', lw=1, label="Ground Truth")
+plt.imshow(cell)
 plt.show()
