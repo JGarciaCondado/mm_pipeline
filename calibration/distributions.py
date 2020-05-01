d1533bbfb8f768448688f0bf63bc43152dce5da2
Jorge Garcia Condado <jg811@cam.ac.uk>
2020-05-01 12:36:17 +0200

    Add guassian fit to parameter extraction of l and theta

--- calibration/distributions.py
+++ calibration/distributions.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from tifffile import imread
 from extraction import extract_params
+from scipy.stats import norm
 
 directory = "Data"
 params = []
@@ -17,12 +18,29 @@ for f in os.listdir(directory):
         pass
 
 params = np.array(params)
+mu_l, std_l = norm.fit(params[:, 0])
 bins = np.arange(0, 6, 0.25)
-plt.hist(params[:, 0], bins)
+plt.hist(params[:, 0], bins, density=True)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu_l, std_l)
+plt.plot(x, p, 'k', linewidth=2)
+plt.xlabel('l (micrometers)')
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu_l, std_l)
+plt.title(title)
 plt.show()
 params[:, 1] = [abs(params[i,1]/params[i,0]) if abs(params[i,1]/params[i,0]) < 10 else 10 for i in range(len(params[:,1]))]
 bins = np.arange(0, 10.5, 0.5)
 plt.hist(params[:, 1], bins)
+plt.xlabel('ratio R/l')
 plt.show()
-plt.hist(params[:, 2])
+mu_theta, std_theta = norm.fit(params[:, 2])
+plt.hist(params[:, 2], density=True)
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu_theta, std_theta)
+plt.plot(x, p, 'k', linewidth=2)
+plt.xlabel('theta (º)')
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu_theta, std_theta)
+plt.title(title)
 plt.show()
