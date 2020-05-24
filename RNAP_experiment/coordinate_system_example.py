64f4648bab4bf42e1a52f5bdd4c04de3f3f5be1b
Jorge Garcia Condado <jg811@cam.ac.uk>
2020-05-24 13:37:00 +0200

    Add distance of top quantiles

--- RNAP_experiment/coordinate_system_example.py
+++ RNAP_experiment/coordinate_system_example.py
@@ -25,7 +25,7 @@ pm = 0.11
 
 # Load cell
 directory = 'data/extracted_cells/'
-cell = np.load(directory+os.listdir(directory)[3])
+cell = np.load(directory+sorted(os.listdir(directory))[-30])
 
 # Load model
 model = tf.keras.models.load_model('segmnet')
@@ -36,13 +36,20 @@ pixelated_mask = segment_cell(cell[0], model)
 # Second fluorescence channel
 cell_fl = cell[1].astype(np.float32)
 
+# Show two channels
+plt.subplot(1,2,1)
+plt.imshow(cell[0])
+plt.subplot(1,2,2)
+plt.imshow(cell[1])
+plt.show()
+
 # Normalize with values within bacteria
 bacteria_px = np.argwhere(pixelated_mask==1.0)
 bac_x, bac_y = zip(*bacteria_px)
 cell_fl = (cell_fl-np.min(cell_fl[bac_x, bac_y]))/(np.nanmax(cell_fl[bac_x, bac_y])-np.nanmin(cell_fl[bac_x, bac_y]))
 
 # Filtered cell with weiner filter
-cell_filtered = wiener(cell_fl, gkern(30, 4), 100)
+cell_filtered = wiener(cell_fl, gkern(30, 4), 20)
 
 # Display fluorescence channel
 plt.subplot(1,2,1)
@@ -73,7 +80,7 @@ plt.plot(smoothed_boundary[:,0], smoothed_boundary[:, 1], color='r')
 centerline = get_centerline(Polygon(smoothed_boundary).buffer(0))
 centerline = clean_centerline(centerline)
 centerline = debranch_centerline(centerline)
-spline = np.sort(np.array(centerline[0], dtype=([('xcoor', np.float64), ('ycoor', np.float64)])),order='ycoor')
+spline = np.sort(np.array(centerline[0], dtype=([('xcoor', float), ('ycoor', float)])),order='ycoor')
 spline = [point for point in spline]
 spline_x, spline_y = zip(*spline)
 spline = LineString(spline)
@@ -104,9 +111,52 @@ for px in bacteria_px:
 
 # Plot channels of interest
 plt.subplot(1,3,1)
-plt.imshow(cell_fl*volume_elements, vmax=100)
-plt.subplot(1,3,2)
 plt.imshow(cell_fl)
+plt.subplot(1,3,2)
+cell_fl = cell_fl*volume_elements
+plt.imshow(cell_fl, vmax=100)
 plt.subplot(1,3,3)
-plt.imshow(cell_filtered*volume_elements, vmax=3)
+cell_filtered = cell_filtered*volume_elements
+plt.imshow(cell_filtered, vmax=4)
+plt.show()
+
+
+# Sort pixels by values
+px_val = [value for value in cell_filtered.flatten() if not np.isnan(value)]
+plt.hist(px_val)
+plt.show()
+
+# R values of top quantile
+px_val_sort = np.sort(px_val)
+top_quantile = px_val_sort[-int(0.25*len(px_val_sort)):]
+plt.hist(top_quantile)
+plt.show()
+
+distance = []
+for index in np.argwhere(cell_filtered>=top_quantile[0]):
+    coor = Point((index[1], index[0]))
+    # Calculate distance to pixel from centerline
+    d2c = pm*spline.distance(coor)/r
+    distance.append(d2c)
+
+print(np.mean(distance))
+plt.hist(distance)
+plt.show()
+
+
+# R values of botoom quantile
+px_val_sort = np.sort(px_val)
+bttm_quantile = px_val_sort[:int(0.25*len(px_val_sort))]
+plt.hist(bttm_quantile)
+plt.show()
+
+distance = []
+for index in np.argwhere(cell_filtered<=bttm_quantile[-1]):
+    coor = Point((index[1], index[0]))
+    # Calculate distance to pixel from centerline
+    d2c = pm*spline.distance(coor)/r
+    distance.append(d2c)
+
+print(np.mean(distance))
+plt.hist(distance)
 plt.show()
